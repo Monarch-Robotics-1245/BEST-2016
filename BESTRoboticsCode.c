7f183439b78a227c6194a0fe07c851d4c9af5ac4
Jackson <jackson@5-scotts.com>
2016-10-07 13:39:01 -0600

    Annotated Control Buttons
    Went through and annotated the controls for all of the functions.

--- BESTRoboticsCode.c
+++ BESTRoboticsCode.c
@@ -23,9 +23,9 @@
 |*    Motor - Port 2          rightMotor          VEX Motor           Right motor                     *|
 |*    Motor - Port 3          leftMotor           VEX Motor           Left motor                      *|
 |*    Motor - Port 4          boomMotor           VEX Motor           Boom Extension Control          *|
-|*		Motor - Port 6					cornGrabber					Servo								Corn Grabber										*|
-|*		Motor - Port 7					seedControl					Servo								Seed Door Control								*|
-|*		Motor - Port 8					cornDoor						Servo								Corn Bay Door Control						*|
+|*    Motor - Port 6          cornGrabber         Servo               Corn Grabber                    *|
+|*    Motor - Port 7          seedControl         Servo               Seed Door Control               *|
+|*    Motor - Port 8          cornDoor            Servo               Corn Bay Door Control           *|
 So seed needs to stay closed until opened,so we can drive along to planting
 Then, when opened, needs to stay opened until closed so we can drive along planting
 Then, when closed, needs to stay closed until opened so we can drive back and get more seeds
@@ -44,11 +44,11 @@ task main()
 	while(1 == 1)
 	{
 		//Driving Motor Control
-		motor[leftMotor] = vexRT[Ch3] / 2;
-		motor[rightMotor] = vexRT[Ch2] / 2;
+		motor[leftMotor] = vexRT[Ch3] / 2;//Left Joystick Up/Down
+		motor[rightMotor] = vexRT[Ch2] / 2;//Right Joystick Up/Down
 
 		//Corn Door  Control
-		if(vexRT[Btn8U] == 1 && vexRT[Btn8R] == 1)
+		if(vexRT[Btn8U] == 1 && vexRT[Btn8R] == 1)//Right Side Up/Right Buttons
 		{
 			cornState *= -1;
 			delay(200);
@@ -56,7 +56,7 @@ task main()
 		motor[cornDoor] = cornState;
 
 		//Seed control
-		if(vexRT[Btn8D] == 1 && vexRT[Btn8L] == 1)
+		if(vexRT[Btn8D] == 1 && vexRT[Btn8L] == 1)//Right Side Down/Left Buttons
 		{
 			seedState *= -1;
 			delay(200);
@@ -65,7 +65,7 @@ task main()
 		motor[seedControl] = seedState;
 
 		//Corn Grabber Control
-		if(vexRT[Btn7U] == 1 && vexRT[Btn7L] == 1)
+		if(vexRT[Btn7U] == 1 && vexRT[Btn7L] == 1)//Left Side Up/Left Buttons
 		{
 			//motor[cornGrabber] = 120; //open
 			if(gripState < openLimit){
@@ -74,7 +74,7 @@ task main()
 			}
 
 		}
-		else if(vexRT[Btn7D] == 1 && vexRT[Btn7R] == 1)
+		else if(vexRT[Btn7D] == 1 && vexRT[Btn7R] == 1)//Left Side Down/Right Buttons
 		{
 			//motor[cornGrabber] = -120; //close
 			if(gripState > closeLimit){
@@ -91,11 +91,11 @@ task main()
 		*/
 
 		//Boom Control
-		if(vexRT[Btn6U] == 1 || vexRT[Btn5U] == 1)
+		if(vexRT[Btn6U] == 1 || vexRT[Btn5U] == 1)//Upper Shoulder Buttons
 		{
 			motor[boomMotor] = 160;
 		}
-		else if(vexRT[Btn6D] == 1 || vexRT[Btn5D] == 1)
+		else if(vexRT[Btn6D] == 1 || vexRT[Btn5D] == 1)//Lower Shoulder Buttons
 		{
 			motor[boomMotor] = -160;
 		}
